d27c5f5bef3cdd73ef2f29614482a6b68baef023
Student <student@lab-315-020.cs.csubak.edu>
2018-02-23 14:52:29 -0800

    two class files

--- randiA.cpp
+++ randiA.cpp
@@ -1,4 +1,34 @@
 //Project: Shape
 //Name: Randi Al.
 //Last modified: 2/23/18
-//Test 1 test 2
+
+#include <math.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+struct Shape {
+	float vel[2];
+	float pos[2];
+	
+	Shape(int posX, int posY) {
+		pos[0] = posX;
+		pos[1] = posY;
+		vel[0] = 0.0f;
+		vel[1] = 0.0f;
+	}
+
+	void updatePosition() {
+	    	pos[0] += pos[0] + vel[0];
+	    	pos[1] += pos[1] + vel[1];
+	}
+
+	void changeVelocity(int x, int y) {
+		vel[0] = x;
+		vel[1] = y;	
+	}
+
+};
+
+struct Collision {
+	
+};

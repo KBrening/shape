e36a1373d0094ff9678ad58fa837f051b5cf9472
randi <randi>
2018-04-21 07:03:19 -0700

    added the four shapes

--- randiA.cpp
+++ randiA.cpp
@@ -7,6 +7,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+
+#define PI 3.14159265358979323846264338327950
 
 double firstFunc() {
     static double acc = 0.0;
@@ -20,7 +25,6 @@ double firstFunc() {
 	  (double)(second.tv_nsec - first.tv_nsec) / 1e9;
     return acc;
 }
-
 double secondFunc() {
     static double acc = 0.0;
     struct timespec first, second;
@@ -32,34 +36,86 @@ double secondFunc() {
 	  (double)(second.tv_nsec - first.tv_nsec) / 1e9;
     return acc;
 }
-	 
+void printToConsole() {
+	printf("Test\n");fflush(stdout);
+}
 struct Shape {
-	float vel[2];
-	float pos[2];
-	
-	Shape(int posX, int posY) {
-		pos[0] = posX;
-		pos[1] = posY;
-		vel[0] = 0.0f;
-		vel[1] = 0.0f;
+	float vertices[32][2];
+	int n;
+	Shape() {
+		n = 32;
+		float angle = 0.0f;
+		float increment = (2.0 * PI)/ (float)n;
+		for (int i = 0; i < n; i++) {
+			vertices[i][0] = sin(angle);
+			vertices[i][1] = cos(angle);
+			angle += increment;
+		}
+	}
+	void updatePosition() { }
+
+	void changeVelocity() { }
+
+	void Square(int sideLength) {
+		glBegin(GL_QUADS);
+			glVertex2i(-sideLength, 0);
+			glVertex2i(-sideLength, 2*sideLength);
+			glVertex2i( sideLength, 2*sideLength);
+			glVertex2i( sideLength, 0);
+		glEnd();
 	}
 
-	void updatePosition() {
-	    	pos[0] += pos[0] + vel[0];
-	    	pos[1] += pos[1] + vel[1];
+	void Circle(float radius) {
+		glBegin(GL_TRIANGLE_FAN);
+		for (int i = 0; i < n; i++) {
+			glVertex2f(vertices[i][0]*radius, vertices[i][1]*radius);
+		}
+		glEnd();
 	}
 
-	void changeVelocity(int x, int y) {
-		vel[0] = x;
-		vel[1] = y;	
+	void Kite(int baseModifier) {
+		glBegin(GL_QUADS);
+			glVertex2i(-baseModifier, 2*baseModifier);
+			glVertex2i(0, 3*baseModifier);
+			glVertex2i(baseModifier, 2*baseModifier);
+			glVertex2i(0,0);
+		glEnd();
+
 	}
 
-};
+	void Star(int scale) {
+		glBegin(GL_TRIANGLE_FAN);
+			glVertex2i(0, 4*scale);
+			glVertex2i(-7*scale, 0);
+			glVertex2i(-4*scale, 6*scale);
+			glVertex2i(-10*scale, 10*scale);
+			glVertex2i(-2*scale, 10*scale);
+			glVertex2i(0, 16*scale);
+			glVertex2i(2*scale, 10*scale);
+			glVertex2i(10*scale, 10*scale);
+			glVertex2i(4*scale, 6*scale);
+			glVertex2i(7*scale, 0);
+		glEnd();
+	}
+
+} shape;
 
 struct Collision {
 	
 };
-
-void printToConsole() {
-	printf("Test\n");fflush(stdout);
+void drawSquare(int sideLength) 
+{
+	shape.Square(sideLength);
+}
+void drawCircle(float radius) 
+{
+	shape.Circle(radius);
+}
+void drawKite(int baseModifier) 
+{
+	shape.Kite(baseModifier);
+}
+void drawStar(int scale) 
+{
+	shape.Star(scale);
 }
